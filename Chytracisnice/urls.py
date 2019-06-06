d785c2cc37f6e17ab3ac9387106c1f4d808f1eb3
Caebix <ma.davidlangr@gmail.com>
2019-06-06 11:52:01 +0200

    blog 3 update

--- Chytracisnice/urls.py
+++ Chytracisnice/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('eshopy_chatbot_vzor', views.eshopyfreevzor, name='eshopyfreevzor'),
     path('5_kroku_k_vytvoreni_chatbota', views.blog1, name='blog1'),
     path('Jak_pridat_messenger_chatbot_na_web', views.blog2, name='blog2'),
-    path('Chatbot-pro-eshopy-nabidne-nesplni', views.blog3, name='blog3'),
+    path('Chatbot-pro-eshopy-co-mi-nabidne-a-co-nesplni', views.blog3, name='blog3'),
     path('sitemap.xml', views.sitemap, name='sitemap'),
 ]
